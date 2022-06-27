b74e3341e213765369b4457afdda17e0ac113a99
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2022-06-27 18:30:56 -0400

    Add project files.

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private Data.DataContext _context;
+
+        public EmployeeController(Data.DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> Get(int Id)
+        {
+            var employee = await _context.Employees.FindAsync(Id);
+            if (employee == null)
+            {
+                return BadRequest("Employee Not Found");
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Employee>>> UpdateEmployee(Employee request)
+        {
+            var employee = await _context.Employees.FindAsync(request.Id);
+            if (employee == null)
+            {
+                return BadRequest("Employee Not Found");
+            }
+
+            employee.Name = request.Name;
+            employee.Salary = request.Salary;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ EmployeeManagement/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+     }
+}
