4f8feee4ebb63ac68c2dc74b43d341bacb72a4ac
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2022-07-01 14:40:36 -0400

    Connection update for project

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,14 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using EmployeeManagement.Data;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-var AzureConnectionString = builder.Configuration["AzureSQL"];
-// Add services to the container.
+
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+var keyVaultUrl = "https://employeemanagementkeys.vault.azure.net/";
+
+var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+var secret = client.GetSecret("AzureSQL").Value.Value;
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(AzureConnectionString);
+    options.UseSqlServer(secret);
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
